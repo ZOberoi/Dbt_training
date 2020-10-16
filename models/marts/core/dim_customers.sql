a5559a90c059b0880b6d24d518448ac8465530c6
zain oberoi <oberoi.zain@bcg.com>
2020-10-16 20:01:38 +0000

    -- fixed a bug with wrong var names used

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -11,7 +11,7 @@ customer_orders as (
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
         count(order_id) as number_of_orders,
-        sum(amount) as lifetime_value_total
+        sum(amount) as lifetime_payments_total
     from orders
     group by 1
 ),
@@ -32,12 +32,11 @@ final as (
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
-        customer_orders.lifetime_value_total,
         customer_orders.lifetime_payments_total,
-        lifetime_order_value.lifetime_value_balance
+        coalesce(lifetime_order_value.lifetime_value_balance, 0)
     from customers
     left join customer_orders using (customer_id)
-    left join lifetime_value using (customer_id)
+    left join lifetime_order_value using (customer_id)
     where number_of_orders > 0
 )
 select * from final
